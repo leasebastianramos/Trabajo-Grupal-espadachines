03e73e64539eca53d08ea6a936805291ab73c9fb
Tin <jmdiazlugo+git@gmail.com>
2023-10-29 20:16:22 -0300

    Se agrega ventana emergente con opciones

--- Obj3_Espadachines.py
+++ Obj3_Espadachines.py
@@ -15,7 +15,7 @@ def texto1era_ventana(miFrame,ventana):
     texto_bienvenida.place(x=20, y=50)
     texto_para_cerrar= Label(miFrame, text="Para continuar presione continuar, de lo contrario cierre la ventana.")
     texto_para_cerrar.place(x=20, y=80)
-    botonEnviar = Button(ventana,text="continuar")
+    botonEnviar = Button(ventana,text="continuar", command=cifrados)
     botonEnviar.place (x=200, y=150)
     texto= Label(miFrame, text="Construido por : Leandro Sebastian Ramos")
     texto.place(x=20, y=400)
@@ -25,9 +25,35 @@ def texto1era_ventana(miFrame,ventana):
     nombre3.place(x=110, y=440)
     nombre4= Label(miFrame, text="Iñaki Vydra")
     nombre4.place(x=110, y=460)
-    
+
+def cifrados():
+    nuevaVentana = Toplevel()
+    nuevaVentana.title("Opciones de Cifrado")
+    nuevaVentana.iconbitmap("espadachines.ico")
+    nuevaVentana.resizable(0,0)
+    marcoOpciones = Frame(nuevaVentana, width=300, height=300)
+    marcoOpciones.pack()
+    texto_mensaje = Label(marcoOpciones, text="Ingrese un mensaje")
+    texto_mensaje.grid(row=0, column=0)
+    entrada_mensaje = Entry(marcoOpciones)
+    entrada_mensaje.grid(row=0, column=1)
+    texto_clave = Label(marcoOpciones, text='Clave')
+    texto_clave.grid(row=1, column=0)
+    entrada_clave = Entry(marcoOpciones)
+    entrada_clave.grid(row=1, column=1)
+    boton_cifrado_cesar = Button(marcoOpciones, text="Cifrar mensaje César")
+    boton_cifrado_cesar.grid(row=2, column=0)
+    boton_descifrado_cesar = Button(marcoOpciones, text="Descifrar mensaje César")
+    boton_descifrado_cesar.grid(row=3, column=0)
+    boton_cifrado_atbash = Button(marcoOpciones, text="Cifrar mensaje Atbash")
+    boton_cifrado_atbash.grid(row=2, column=1)
+    texto_vacio = Label(marcoOpciones)
+    boton_descifrado_atbash = Button(marcoOpciones, text="Descifrar mensaje Atbash")
+    boton_descifrado_atbash.grid(row=3, column=1)
+
 def main():
     ventana1_programa()
 
-main()
+if __name__=='__main__':
+    main()
     
